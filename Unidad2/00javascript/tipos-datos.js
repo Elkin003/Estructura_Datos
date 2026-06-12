4a165ec31f24567018d6f88c3d4e26af3404c63d
Elkin003 <elkinjjm@gmail.com>
2026-06-12 13:08:08 -0500

    Feat: Learning Javascript

---
+++ Unidad2/00javascript/tipos-datos.js
@@ -0,0 +1,37 @@
+// PRIMITIVOS
+
+// string
+let nombre = 'María'
+
+// numerico
+let edad = 25
+
+// booleanos
+let esMayorDeEdad = true
+
+// null
+let noHayValor = null
+
+// undefined
+let noDefinido = undefined
+
+// symbol valor unico
+let simboloUnico = Symbol('unico')
+
+// datos gigantes bigint
+let numeroGrande = 2n
+
+
+// COMPLETOS
+
+// object
+let carro = {
+    marca:'Tesla',
+    modelo:'Model S'
+}
+
+// arrays
+let frutas  = ['manzana', 'pera', 'piña']
+
+// fuctions
+function saludar(){}
